2b6f509326407481c07878b7d6811db789b44d04
Gabriel Araujo Dantas <gabriel.araujo.dantas@gmail.com>
2024-04-30 19:39:48 +0000

    Changing anotation

--- src/main/java/com/example/GithubWebhook/web/GithubWebhookController.java
+++ src/main/java/com/example/GithubWebhook/web/GithubWebhookController.java
@@ -1,6 +1,5 @@
 package com.example.GithubWebhook.web;
 
-import java.util.Map;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -8,14 +7,12 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
-
 @RestController
 @RequestMapping("/webhook")
 public class GithubWebhookController {
     
     @PostMapping("/event")
-    public ResponseEntity<String> receiveEvent(@RequestBody String payload){
+    public ResponseEntity<String> receiveEvent(@org.springframework.web.bind.annotation.RequestBody String payload){
         System.out.println(payload);
         return ResponseEntity.status(HttpStatus.ACCEPTED).body("Accepted");
     }
